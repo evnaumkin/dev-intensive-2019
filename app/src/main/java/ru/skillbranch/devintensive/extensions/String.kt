fa5766d60f83bf512e501542d458c52785fe9a28
Eugene Naumkin <sunora@mail.ru>
2019-07-11 20:00:39 +0300

    hometask_2 correction_1

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -4,7 +4,7 @@ fun String.truncate(value:Int = 16): String {
     var tmpStr = this.trim()
     return when {
         tmpStr.length <= value -> tmpStr
-        else -> tmpStr.substring(0..value).trimEnd().plus("...")
+        else -> tmpStr.substring(0,value).trimEnd().plus("...")
     }
 }
 fun String.stripHtml(): String {
