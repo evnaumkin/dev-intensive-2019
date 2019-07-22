5f61f9b3c2e1c88563c7917026f7fed7da8c951a
Eugene Naumkin <sunora@mail.ru>
2019-07-22 22:53:56 +0300

    hometask_3

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -0,0 +1,23 @@
+package ru.skillbranch.devintensive.extensions
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Rect
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+fun Activity.hideKeyboard() {
+    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+}
+
+fun Activity.isKeyboardOpen(): Boolean {
+    val view = findViewById<View>(android.R.id.content)
+    val rect = Rect()
+    view.getWindowVisibleDisplayFrame(rect)
+    val visible = view.height > rect.height()
+    return visible
+}
+
+fun Activity.isKeyboardClosed() = !isKeyboardOpen()
